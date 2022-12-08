c66cbf2d4f880e5d7aadeefbd660d0765f04e3a9
Leennguyen <nguyenthilinh26112002@gmail.com>
2022-12-08 11:36:30 +0700

    Revert "ok"
    This reverts commit 2585ac1f94a55cd21667bf2f9a208218dae3b072.

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react'
 
 export default function Homepage() {
   return (
     <div>
       <h1> Hello everyone. My name's Linh</h1>
       <p>PNV</p>
-      <p>ok! I see</p>
-      <p>hhhhhhhhhhhhhh</p>
     </div>
-  );
+  )
 }
