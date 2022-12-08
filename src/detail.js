fe512e685fe90dc30d94561b589bc77e7b2a525f
Leennguyen <nguyenthilinh26112002@gmail.com>
2022-12-08 10:57:42 +0700

    hihihihi

--- src/detail.js
+++ src/detail.js
@@ -1,1 +1,8 @@
 const mockAPI = "https://62904135665ea71fe12f6eef.mockapi.io/products";
+import React from 'react'
+
+export default function detail() {
+  return (
+    <div>kjbscoisbcksb</div>
+  )
+}
